47a564fb82ce49e0fe3248a4e4d350f2ad984721
Samyak S Sarnayak <samyak201@gmail.com>
2020-06-25 17:19:20 +0530

    More sounds and search feature for sounds

---
+++ database.py
@@ -0,0 +1,93 @@
+import os.path
+import shutil
+
+import whoosh.index as index
+import whoosh.fields as fields
+import whoosh.qparser as qparser
+
+schema = fields.Schema(keywords=fields.TEXT(stored=False),
+                       audio_url=fields.STORED(),
+                       title=fields.STORED(),
+                       id=fields.STORED()
+                       )
+
+if os.path.exists("index"):
+    shutil.rmtree("index")
+os.mkdir("index")
+ix = index.create_in("index", schema)
+
+writer = ix.writer()
+writer.add_document(
+    keywords="kono dio da",
+    audio_url="https://www.myinstants.com/media/sounds/kono-dio-da99.mp3",
+    title="Kono Dio Da!",
+    id="kono-dio-da99")
+writer.add_document(
+    keywords="za warudo the world",
+    audio_url="https://www.myinstants.com/media/sounds/za-warudo-stop-time-sound.mp3",
+    title="Za Warudo!",
+    id="za-warudo-stop-time-sound")
+writer.add_document(
+    keywords="wryyy wry wryy wryyyy",
+    audio_url="https://www.myinstants.com/media/sounds/dio-wryyy.mp3",
+    title="Wryyyyy",
+    id="dio-wryyy")
+writer.add_document(
+    keywords="roundabout to be continued",
+    audio_url="https://www.myinstants.com/media/sounds/untitled_1071.mp3",
+    title="Roundabout",
+    id="untitled_1071")
+writer.add_document(
+    keywords="oh no",
+    audio_url="https://www.myinstants.com/media/sounds/oh-no_7.mp3",
+    title="Ohh noo",
+    id="oh-no_7")
+writer.add_document(
+    keywords="yes yes yes yes!",
+    audio_url="https://www.myinstants.com/media/sounds/yes-yes-yes-yes-yes.mp3",
+    title="YES YES YES YES!",
+    id="yes-yes-yes-yes-yes")
+writer.add_document(
+    keywords="no no no no no",
+    audio_url="https://www.myinstants.com/media/sounds/jotaro-no.mp3",
+    title="NO NO NO NO NO!",
+    id="jotaro-no")
+writer.add_document(
+    keywords="dero lelo rero lero",
+    audio_url="https://www.myinstants.com/media/sounds/rero-rero-rero.mp3",
+    title="dero dero dero",
+    id="rero-rero-rero")
+writer.add_document(
+    keywords="ay ay ay awaken my masters",
+    audio_url="https://www.myinstants.com/media/sounds/jojos-bizarre-adventure-ay-ay-ay-ay-_-sound-effect.mp3",
+    title="AWAKEN",
+    id="jojos-bizarre-adventure-ay-ay-ay-ay-_-sound-effect")
+writer.add_document(
+    keywords="nice",
+    audio_url="https://www.myinstants.com/media/sounds/joseph-joestar-nice.mp3",
+    title="nice",
+    id="joseph-joestar-nice")
+writer.add_document(
+    keywords="nigerundayo run away",
+    audio_url="https://www.myinstants.com/media/sounds/joestar-run.mp3",
+    title="NIGERUNDAYO",
+    id="joestar-run")
+writer.add_document(
+    keywords="yes i am tsk tsk",
+    audio_url="https://www.myinstants.com/media/sounds/yes-i-am.mp3",
+    title="Yes I am!",
+    id="yes-i-am")
+writer.add_document(
+    keywords="kira yoshikage theme",
+    audio_url="https://www.myinstants.com/media/sounds/yoshikage-kira-theme-ringtone.mp3",
+    title="Kira Yoshikage Theme",
+    id="yoshikage-kira-theme-ringtone")
+writer.commit()
+
+parser = qparser.QueryParser("keywords", ix.schema)
+
+if __name__ == "__main__":
+    searcher = ix.searcher()
+    myquery = parser.parse("kono")
+    results = searcher.search(myquery)
+    print(results)
--- reply.py
+++ reply.py
@@ -5,11 +5,11 @@ import sys
 import time
 import json
 import pprint
-
-TOKEN = os.getenv("TOKEN")
-
 import requests
 
+from database import ix, parser
+
+TOKEN = os.getenv("TOKEN")
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 message_responses = {
     "who are you?": "Kono Dio Da!",
@@ -17,11 +17,6 @@ message_responses = {
     "what are you?": "Kono Dio Da!",
     "what are you": "Kono Dio Da!",
     "hi": "_Oh, you're approaching me?_",
-}
-audios = {
-    "Kono Dio Da": ["https://www.myinstants.com/media/sounds/kono-dio-da99.mp3",
-                    "Kono Dio Da!",
-                    "kono-dio-da99"]
 }
 default_message = "Wryyyyyyy"
 LAST_UPDATE_FILE = "last_id.txt"
@@ -39,15 +34,10 @@ def send_message(text, chat_id):
 
     data = response.json()
 
-def send_inline_query(query, query_id):
-    results = []
-    for value in audios.values():
-        results.append({
-            "type": "audio",
-            "id": value[2],
-            "audio_url": value[0],
-            "title": value[1]
-        })
+def send_inline_query(query, query_id, searcher):
+    results = list(map(dict, searcher.search(parser.parse(query+"*"))))
+    list(map(lambda x: x.update({"type": "audio"}), results))
+    pprint.pprint(results)
     response = requests.post(f"{BASE_URL}/answerInlineQuery",
                              json={
                                 "inline_query_id": query_id,
@@ -60,32 +50,34 @@ update_url = "{}/getUpdates".format(BASE_URL)
 
 print("Starting...")
 
-while True:
-    print(".", end="")
-    update_params = {"offset": last_update, "timeout": 5}
-    # headers = {'Prefer': 'wait=120'}
-    headers = {}
-    response = requests.get(url=update_url, params=update_params, headers=headers)
+with ix.searcher() as searcher:
+    while True:
+        print(".", end="")
+        update_params = {"offset": last_update, "timeout": 5}
+        # headers = {'Prefer': 'wait=120'}
+        headers = {}
+        response = requests.get(url=update_url, params=update_params, headers=headers)
 
-    if response.status_code == 200:
-        data = response.json()
+        if response.status_code == 200:
+            data = response.json()
 
-        for update in data["result"]:
-            pprint.pprint(update)
-            if "message" in update and "text" in update["message"] and not update["message"]["from"]["is_bot"]:
-                text = update["message"]["text"].lower()
-                if text in message_responses:
-                    send_message(message_responses[text], update["message"]["chat"]["id"])
-                else:
-                    send_message(default_message, update["message"]["chat"]["id"])
-            if "inline_query" in update:
-                send_inline_query(update["inline_query"]["query"],
-                                  update["inline_query"]["id"])
-            with open(LAST_UPDATE_FILE, "wt") as f:
-                last_update = update["update_id"] + 1
-                f.write(str(last_update))
+            for update in data["result"]:
+                pprint.pprint(update)
+                if "message" in update and "text" in update["message"] and not update["message"]["from"]["is_bot"]:
+                    text = update["message"]["text"].lower()
+                    if text in message_responses:
+                        send_message(message_responses[text], update["message"]["chat"]["id"])
+                    else:
+                        send_message(default_message, update["message"]["chat"]["id"])
+                if "inline_query" in update:
+                    send_inline_query(update["inline_query"]["query"],
+                                      update["inline_query"]["id"],
+                                      searcher)
+                with open(LAST_UPDATE_FILE, "wt") as f:
+                    last_update = update["update_id"] + 1
+                    f.write(str(last_update))
 
-    elif response.status_code != 304:
-        time.sleep(60)
+        elif response.status_code != 304:
+            time.sleep(60)
 
-    time.sleep(0.1)
+        time.sleep(0.1)
