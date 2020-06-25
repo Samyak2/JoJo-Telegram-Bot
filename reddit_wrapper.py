bfaf93b9e686ba284fcb6a789fa02aadde88c85a
Samyak S Sarnayak <samyak201@gmail.com>
2020-06-25 20:28:32 +0530

    Code refactor/cleanup, better output messages

--- reddit_wrapper.py
+++ reddit_wrapper.py
@@ -1,17 +1,16 @@
-import praw
-import json
+import os
 from random import randint
+import praw
+from dotenv import load_dotenv
+load_dotenv()
 
-with open ('.secret-keys.json') as f:
-    data = json.load(f)
-
-client_id = data["client_id"]
-client_secret = data["client_secret"]
-user_agent = data["user_agent"]
+client_id = os.environ.get("CLIENT_ID")
+client_secret = os.environ.get("CLIENT_SERVER")
+user_agent = os.environ.get("USER_AGENT")
 
 reddit = praw.Reddit(client_id=client_id,
-        client_secret=client_secret,
-        user_agent=user_agent)
+                     client_secret=client_secret,
+                     user_agent=user_agent)
 subreddit = reddit.subreddit('shitpostcrusaders')
 
 top_100_posts = []
@@ -20,7 +19,7 @@ def return_post():
     for submission in subreddit.top(limit=100):
         top_100_posts.append(submission.url)
 
-    post = randint(0,99)
+    post = randint(0, 99)
 
     return top_100_posts[post]
 
--- reply.py
+++ reply.py
@@ -1,12 +1,11 @@
-from database import ix, parser
-import requests
 import pprint
-import json
 import time
 import sys
 import os
-from test import return_post
+import requests
 from dotenv import load_dotenv
+from reddit_wrapper import return_post
+from database import ix, parser
 load_dotenv()
 
 
@@ -40,49 +39,65 @@ LAST_UPDATE_FILE = "last_id.txt"
 with open(LAST_UPDATE_FILE, "rt") as f:
     last_update = int(f.read()) + 1
 
-
 def send_message(text, chat_id):
     url = "{}/sendMessage".format(BASE_URL)
-    params = {"chat_id": chat_id,
-            "text": text,
-            "parse_mode": "Markdown"
-            }
+    params = {
+        "chat_id": chat_id,
+        "text": text,
+        "parse_mode": "Markdown"
+    }
     response = requests.get(url=url, params=params)
-    pprint.pprint(data)
+    if response.status_code != requests.codes.ok: # pylint: disable=no-member
+        print("*"*20, "ERROR", "*"*20)
+        pprint.pprint(response.json())
+        print("*"*45)
+    else:
+        print(f"Sent message to chat_id {chat_id}, replying to '{text}'")
 
 def send_gif_response(text, chat_id):
     url = "{}/sendAnimation".format(BASE_URL)
     params = {
-            "chat_id": chat_id,
-            "animation": gifs,
-            }
+        "chat_id": chat_id,
+        "animation": gifs,
+    }
     response = requests.get(url=url, params=params)
-    data = response.json()
-    pprint.pprint(data)
+    if response.status_code != requests.codes.ok: # pylint: disable=no-member
+        print("*"*20, "ERROR", "*"*20)
+        pprint.pprint(response.json())
+        print("*"*45)
+    else:
+        print(f"Sent GIF to chat_id {chat_id}, replying to '{text}'")
 
 def send_meme_response(text, chat_id):
     url = "{}/sendMessage".format(BASE_URL)
     meme = return_post()
     print(meme)
     params = {
-            "chat_id": chat_id,
-            "text": meme,
-            }
+        "chat_id": chat_id,
+        "text": meme,
+    }
     response = requests.get(url=url, params=params)
-    data = response.json()
-    pprint.pprint(data)
+    if response.status_code != requests.codes.ok: # pylint: disable=no-member
+        print("*"*20, "ERROR", "*"*20)
+        pprint.pprint(response.json())
+        print("*"*45)
+    else:
+        print(f"Sent meme to chat_id {chat_id}, replying to '{text}'")
 
 def send_inline_query(query, query_id, searcher):
     results = list(map(dict, searcher.search(parser.parse(query+"*"))))
     list(map(lambda x: x.update({"type": "audio"}), results))
-    pprint.pprint(results)
     response = requests.post(f"{BASE_URL}/answerInlineQuery",
-            json={
-                "inline_query_id": query_id,
-                "results": results
-                })
-    pprint.pprint(response.json())
-
+                             json={
+                                 "inline_query_id": query_id,
+                                 "results": results
+                                 })
+    if response.status_code != requests.codes.ok: # pylint: disable=no-member
+        print("*"*20, "ERROR", "*"*20)
+        pprint.pprint(response.json())
+        print("*"*45)
+    else:
+        print(f"Sent inline result to query_id {query_id}, with query '{query}'")
 
 update_url = "{}/getUpdates".format(BASE_URL)
 # headers = {'Prefer': 'wait=120'}
@@ -92,6 +107,7 @@ print("Listening...")
 with ix.searcher() as searcher:
     while True:
         print(".", end="")
+        sys.stdout.flush()
         update_params = {"offset": last_update, "timeout": 5}
         # headers = {'Prefer': 'wait=120'}
         headers = {}
@@ -102,25 +118,24 @@ with ix.searcher() as searcher:
             data = response.json()
 
             for update in data["result"]:
-                pprint.pprint(update)
-                if "message" in update and "text" in update["message"] and not update["message"]["from"]["is_bot"]:
+                if "message" in update and \
+                   "text" in update["message"] and not update["message"]["from"]["is_bot"]:
                     text = update["message"]["text"].lower()
+                    print(f"Got message '{text}' from chat_id {update['message']['chat']['id']}")
                     if text in message_responses:
-                        send_message(
-                                message_responses[text], update["message"]["chat"]["id"])
+                        send_message(message_responses[text], update["message"]["chat"]["id"])
                     elif text in gif_responses:
-                        send_gif_response(
-                                text, update["message"]["chat"]["id"])
+                        send_gif_response(text, update["message"]["chat"]["id"])
                     elif text in meme_responses:
-                        send_meme_response(
-                                text, update["message"]["chat"]["id"])
+                        send_meme_response(text, update["message"]["chat"]["id"])
                     else:
                         send_message(default_message,
-                                update["message"]["chat"]["id"])
+                                     update["message"]["chat"]["id"])
                 if "inline_query" in update:
+                    print("Got inline query")
                     send_inline_query(update["inline_query"]["query"],
-                                    update["inline_query"]["id"],
-                                    searcher)
+                                      update["inline_query"]["id"],
+                                      searcher)
                 with open(LAST_UPDATE_FILE, "wt") as f:
                     last_update = update["update_id"] + 1
                     f.write(str(last_update))
