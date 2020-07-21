9087c3bf4790b3b11475705b7adcb160f24f163c
Samyak S Sarnayak <samyak201@gmail.com>
2020-07-22 00:15:30 +0530

    [Refactor] Separate main function, docstrings for all functions
    Finally fixed all pylint warnings!

--- reply.py
+++ reply.py
@@ -14,7 +14,7 @@ TOKEN = os.getenv("TOKEN")
 BASE_URL = "https://api.telegram.org/bot{}".format(TOKEN)
 
 # configuration here
-HELP_TEXT = """Welcome to JoJo Bot, the commands are 
+HELP_TEXT = """Welcome to JoJo Bot, the commands are
                     -- what/who are you?
                     -- hi
                     -- hello there
@@ -72,19 +72,16 @@ gifs = "https://media.giphy.com/media/8JTFsZmnTR1Rs1JFVP/giphy.gif"
 # default message when nothing is matched
 default_message = "Wryyyyyyy! Try /help if you're looking for options."
 
-# to store ID of the last update processed
-LAST_UPDATE_FILE = "last_id.txt"
-with open(LAST_UPDATE_FILE, "rt") as f:
-    last_update = int(f.read()) + 1
-
 # set up requests
 sess = requests.Session()
 retries = urllib3.util.retry.Retry(total=10,
-        backoff_factor=0.1,
-        status_forcelist=[500, 502, 503, 504])
+                                   backoff_factor=0.1,
+                                   status_forcelist=[500, 502, 503, 504])
 sess.mount("https://", requests.adapters.HTTPAdapter(max_retries=retries))
 
 def send_message(text, chat_id):
+    """Sends `text` to chat `chat_id`
+    """
     url = "{}/sendMessage".format(BASE_URL)
     params = {
         "chat_id": chat_id,
@@ -100,6 +97,8 @@ def send_message(text, chat_id):
         print(f"Sent message to chat_id {chat_id}, replying with '{text}'")
 
 def send_gif_response(text, chat_id):
+    """Replies to a message with `text` in chat `chat_id`
+    """
     url = "{}/sendAnimation".format(BASE_URL)
     params = {
         "chat_id": chat_id,
@@ -114,6 +113,8 @@ def send_gif_response(text, chat_id):
         print(f"Sent GIF to chat_id {chat_id}, replying to '{text}'")
 
 def send_meme_response(text, chat_id):
+    """Replies to a message `text` with a random reddit meme in chat `chat_id`
+    """
     url = "{}/sendMessage".format(BASE_URL)
     meme = return_post()
     print(meme)
@@ -130,13 +131,15 @@ def send_meme_response(text, chat_id):
         print(f"Sent meme to chat_id {chat_id}, replying to '{text}'")
 
 def send_inline_query(query, query_id, searcher):
+    """Sends an inline query result by searching for `query` in the database
+    and sends it to `query_id`. `searcher` is a woosh searcher instance for the database
+    """
     results = list(map(dict, searcher.search(parser.parse(query+"*"))))
     list(map(lambda x: x.update({"type": "audio"}), results))
     response = sess.post(f"{BASE_URL}/answerInlineQuery",
-                             json={
-                                 "inline_query_id": query_id,
-                                 "results": results
-                                 })
+                         json={
+                             "inline_query_id": query_id,
+                             "results": results})
     if response.status_code != requests.codes.ok: # pylint: disable=no-member
         print("*"*20, "ERROR", "*"*20)
         pprint.pprint(response.json())
@@ -149,44 +152,55 @@ update_url = "{}/getUpdates".format(BASE_URL)
 
 print("Listening...")
 
-with ix.searcher() as searcher:
-    while True:
-        print(".", end="")
-        sys.stdout.flush()
-        update_params = {"offset": last_update, "timeout": 5}
-        # headers = {'Prefer': 'wait=120'}
-        headers = {}
-        response = sess.get(
-            url=update_url, params=update_params, headers=headers)
+def main():
+    """Main method to handle everything"""
+    # to store ID of the last update processed
+    LAST_UPDATE_FILE = "last_id.txt"
+    with open(LAST_UPDATE_FILE, "rt") as f:
+        last_update = int(f.read()) + 1
 
-        if response.status_code == 200:
-            data = response.json()
+    with ix.searcher() as searcher:
+        while True:
+            print(".", end="")
+            sys.stdout.flush()
+            update_params = {"offset": last_update, "timeout": 5}
+            # headers = {'Prefer': 'wait=120'}
+            headers = {}
+            response = sess.get(
+                url=update_url, params=update_params, headers=headers)
 
-            for update in data["result"]:
-                if "message" in update and \
-                   "text" in update["message"] and not update["message"]["from"]["is_bot"]:
-                    text = update["message"]["text"].lower()
-                    text = text.replace(BOT_TAG, "")
-                    print(f"\nGot message '{text}' from chat_id {update['message']['chat']['id']}")
-                    if text in message_responses:
-                        send_message(message_responses[text], update["message"]["chat"]["id"])
-                    elif text in gif_responses:
-                        send_gif_response(text, update["message"]["chat"]["id"])
-                    elif text in meme_responses:
-                        send_meme_response(text, update["message"]["chat"]["id"])
-                    else:
-                        send_message(default_message,
-                                     update["message"]["chat"]["id"])
-                if "inline_query" in update:
-                    print("\nGot inline query")
-                    send_inline_query(update["inline_query"]["query"],
-                                      update["inline_query"]["id"],
-                                      searcher)
-                with open(LAST_UPDATE_FILE, "wt") as f:
-                    last_update = update["update_id"] + 1
-                    f.write(str(last_update))
+            if response.status_code == 200:
+                data = response.json()
 
-        elif response.status_code != 304:
-            time.sleep(60)
+                for update in data["result"]:
+                    if "message" in update and \
+                       "text" in update["message"] and not update["message"]["from"]["is_bot"]:
+                        text = update["message"]["text"].lower()
+                        text = text.replace(BOT_TAG, "")
+                        print(f"\nGot message '{text}'"
+                              f"from chat_id {update['message']['chat']['id']}")
+                        if text in message_responses:
+                            send_message(message_responses[text], update["message"]["chat"]["id"])
+                        elif text in gif_responses:
+                            send_gif_response(text, update["message"]["chat"]["id"])
+                        elif text in meme_responses:
+                            send_meme_response(text, update["message"]["chat"]["id"])
+                        else:
+                            send_message(default_message,
+                                         update["message"]["chat"]["id"])
+                    if "inline_query" in update:
+                        print("\nGot inline query")
+                        send_inline_query(update["inline_query"]["query"],
+                                          update["inline_query"]["id"],
+                                          searcher)
+                    with open(LAST_UPDATE_FILE, "wt") as f:
+                        last_update = update["update_id"] + 1
+                        f.write(str(last_update))
 
-        time.sleep(0.1)
+            elif response.status_code != 304:
+                time.sleep(60)
+
+            time.sleep(0.1)
+
+if __name__ == "__main__":
+    main()
